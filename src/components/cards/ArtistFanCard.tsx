d03335898ba44ed04e0e6a5aef8f9d66c2b8e4a8
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2024-04-19 00:31:29 +0100

    artist calendar

--- src/components/cards/ArtistFanCard.tsx
+++ src/components/cards/ArtistFanCard.tsx
@@ -1,8 +1,4 @@
 import { Box, Stack, Typography } from "@mui/material";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> main
 import { ArtistFanCardType, UserProp } from "@types";
 import Avatar from "components/Avatar";
 
@@ -14,37 +10,13 @@ export default function ArtistFanCard({ avatarImg, username, points }: UserProp)
         <Box>
           <Typography variant='subtitle1' sx={{ color: "common.black" }}>
             {username}
-<<<<<<< HEAD
-=======
-import { ArtistFanCardType } from "@types";
-import Avatar from "components/Avatar";
-
-export default function ArtistFanCard({ avatar, name, points, date }: ArtistFanCardType) {
-  return (
-    <Stack spacing={3} direction='row' sx={{ cursor: "pointer" }}>
-      <Avatar src={avatar} alt='fan avatars' sx={{ height: 80, width: 80, borderRadius: 1 }} />
-      <Stack spacing={3}>
-        <Box>
-          <Typography variant='subtitle1' sx={{ color: "common.black" }}>
-            {name}
->>>>>>> 907b60ac692199d52a0dfdb2db536c01a786ca99
-=======
->>>>>>> main
           </Typography>
           <Typography variant='subtitle2' sx={{ color: "common.black" }}>
             {points} fan experience points
           </Typography>
         </Box>
         <Typography variant='subtitle2' sx={{ color: "grey.600" }}>
-<<<<<<< HEAD
-<<<<<<< HEAD
           {/* {date} */}Join since 09,2023
-=======
-          {date}
->>>>>>> 907b60ac692199d52a0dfdb2db536c01a786ca99
-=======
-          {/* {date} */}Join since 09,2023
->>>>>>> main
         </Typography>
       </Stack>
     </Stack>
