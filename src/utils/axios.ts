9886565f52bd71d2f01d534d470cd4bf4eba619c
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2024-04-19 10:00:20 +0100

    change url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,7 +5,7 @@ import { HOST_API } from '../config';
 // ----------------------------------------------------------------------
 // ec2-100-24-244-112.compute-1.amazonaws.com
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "http://ec2-100-24-244-112.compute-1.amazonaws.com/api/",
   withCredentials: true
 })
 const axiosInstance = axios.create({
